39e39cfc2629ddac59bd6d85db22bf2e967f7af9
KamuiEllis <nathanellis591@gmail.com>
2024-07-16 20:34:13 -0500

    font page  done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -253,3 +253,47 @@
         </div>
     </div>
 </section>
+
+<section class="bg-white dark:bg-gray-900 vip hidden lg:block">
+    <div
+        class="py-8 mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16"
+    >
+        <div class="flex flex-col justify-center">
+            <h1
+                class="mb-4 text-4xl font-extrabold tracking-tight lg:mt-20 leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-gray-700 header"
+            >
+                Elevate Your Lifestyle with Our <span class='text-yellow-700'>VIP CLUB</span>
+            </h1>
+            <p
+                class="mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-700 normal-text"
+            >
+            Our membership club is exclusively crafted for those who value luxury and prioritize their time. With a dedicated personal assistant, experience unparalleled convenience tailored for the discerning few who demand excellence. Redefine your lifestyle and revel in the pinnacle of luxury and efficiency.
+            </p>
+            <div class="flex flex-col space-y-4 sm:flex-row sm:space-y-0">
+                <!-- <a
+                    href="#"
+                    style="background-color: #48c4c2;"
+                    class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+                >
+                    Get started
+                    <svg
+                        class="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 14 10"
+                    >
+                        <path
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M1 5h12m0 0L9 1m4 4L9 9"
+                        />
+                    </svg>
+                </a> -->
+            </div>
+        </div>
+        <div></div>
+    </div>
+</section>
