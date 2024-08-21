e71f5debad0859ab2b295ed6ad2bfd81f7393de0
KamuiEllis <kamuiellis@gmail.com>
2024-08-20 23:05:40 -0500

    ds

--- src/routes/vip/+page.svelte
+++ src/routes/vip/+page.svelte
@@ -122,10 +122,10 @@
 
 <section class="bg-center bg-no-repeat bg-[url('/img/beach.jpg')] bg-gray-700 bg-blend-multiply">
     <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-        <h2 class="mb-7 text-2xl text-left lg:text-center tracking-tight leading-none text-white md:text-5xl lg:text-5xl">Welcome to the world of personalized luxury with our <span style='color:#FFD700'>VIP CLUB</span></h2>
+        <h2 class="mb-7 text-3xl text-left lg:text-center tracking-tight leading-none text-white md:text-5xl lg:text-5xl">Welcome to the world of personalized luxury with our <span style='color:#FFD700'>VIP CLUB</span></h2>
         <p class="mb-8 text-left lg:text-center text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Step into a world where your time is your most treasured luxury. At the Errand Executer VIP Club, we don’t just offer convenience—we craft an experience that’s tailored to your every need. Imagine the peace of mind that comes with knowing your personal assistant, an integral part of our elite team, is dedicated to managing your tasks with the utmost care and discretion</p>
         <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-            <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+            <a href="https://calendly.com/errandexecuter/30min" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                 Schedule a meeting
                 <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
