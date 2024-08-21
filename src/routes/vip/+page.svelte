61938d428048bfaeb48bac8cdac7dba5258421ca
KamuiEllis <kamuiellis@gmail.com>
2024-08-20 21:55:33 -0500

    changes

--- src/routes/vip/+page.svelte
+++ src/routes/vip/+page.svelte
@@ -122,7 +122,7 @@
 
 <section class="bg-center bg-no-repeat bg-[url('/img/beach.jpg')] bg-gray-700 bg-blend-multiply">
     <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Elevate Your Lifestyle with Our VIP Club</h1>
+        <h2 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Elevate Your Lifestyle with Our VIP Club</h2>
         <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48"> Welcome to the Errand Executer VIP Club, where exclusivity meets
             unparalleled convenience. For just $30,000 every three months,
             enjoy the elite service of having a dedicated personal
