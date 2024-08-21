3c9316e50ba06e3df0f45058f9e9213a70733a27
KamuiEllis <kamuiellis@gmail.com>
2024-08-20 21:28:17 -0500

    changes

--- src/routes/vip/+page.svelte
+++ src/routes/vip/+page.svelte
@@ -102,7 +102,7 @@
 
 {#if loading == false}
     <section class="guest-order-section bg-center bg-no-repeat">
-        <div class="px-4 mx-auto text-center pt-10 lg:py-16">
+        <!-- <div class="px-4 mx-auto text-center pt-10 lg:py-16">
             <h3
                 class="mb-4 text-3xl font-extrabold tracking-tight leading-none text-gray-900 lg:text-6xl header"
             >
@@ -117,7 +117,28 @@
                 assistant, one of our business owners, handling your grocery
                 errands with the utmost discretion and priority.
             </p>
+        </div> -->
+        
+
+<section class="bg-center bg-no-repeat bg-[url('/img/beach.jpg')] bg-gray-700 bg-blend-multiply">
+    <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Elevate Your Lifestyle with Our VIP Club</h1>
+        <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48"> Welcome to the Errand Executer VIP Club, where exclusivity meets
+            unparalleled convenience. For just $30,000 every three months,
+            enjoy the elite service of having a dedicated personal
+            assistant, one of our business owners, handling your grocery
+            errands with the utmost discretion and priority.</p>
+        <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+            <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                Schedule a meeting
+                <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </a>
         </div>
+    </div>
+</section>
+
 
             <div class="mx-1 px-10  overflow-x-scroll">
                 <div class='lg:grid lg:grid-cols-3'>
